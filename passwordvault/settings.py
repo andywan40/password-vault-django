69424cf3777954ac73956695153d577752a9e927
Andrew Wan <52335246+andywan40@users.noreply.github.com>
2023-08-23 14:24:34 -0700

    add allowed hosts

--- passwordvault/settings.py
+++ passwordvault/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['password-vault-django.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['password-vault-django.herokuapp.com', 'passwordvaultnext.vercel.app', '127.0.0.1']
 
 # Application definition
 
